b8c01d21add0eaf4851eb5edfe62d307a8d3d277
Chaotic-Cody <codymessinacs@gmail.com>
2019-04-16 15:40:44 -0400

    remove misc print statement

--- main.c
+++ main.c
@@ -6,10 +6,6 @@
 #include <windows.h>
 #endif
 
-#if defined(_MSC_VER) && (_MSC_VER < 1900)
-#define snprintf _snprintf
-#endif
-
 libusb_device *get_device(libusb_device **devs, uint16_t productID, uint16_t vendorID) {
 
 	struct libusb_device_descriptor desc;
@@ -20,10 +16,12 @@ libusb_device *get_device(libusb_device **devs, uint16_t productID, uint16_t ven
 		ret = libusb_get_device_descriptor(devs[i], &desc);
 		if (ret < 0) {
 			fprintf(stderr, "failed to get device descriptor");
-			return EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		}
 
+		#ifdef DEBUG
 		printf("%04X - %04X\n", desc.idVendor, desc.idProduct);
+		#endif
 
 		if (desc.idVendor == vendorID && desc.idProduct == productID) {
 			return devs[i];
@@ -47,18 +45,21 @@ int main (int argc, char **argv) {
 	uint16_t productID = 0x1B2D;
 	uint16_t vendorID = 0x1B1C;
 
-	// Device we'll br working with
+	// Device we'll be working with
 	libusb_device *dev;
+	libusb_device_handle *dev_handle;
 
 	// Initialize libusb library and get an array of connected devices
 	r = libusb_init(NULL);
 	if (r < 0) {
-		return r;
+		fprintf(stderr, "failed to initialize libusb");
+		exit(EXIT_FAILURE);
 	}
 
 	cnt = libusb_get_device_list(NULL, &devs);
 	if (cnt < 0) {
-		return (int)cnt;
+		fprintf(stderr, "no connected devices");
+		exit(EXIT_FAILURE);
 	}
 
 	dev = get_device(devs, productID, vendorID);
@@ -66,6 +67,30 @@ int main (int argc, char **argv) {
 	// Print whether or not we found a connected device
 	printf((dev != NULL) ? "Device found :)\n" : "Device not found :(\n");
 
+	if (dev == NULL) {
+		fprintf(stderr, "Corsair K95 RGB Platinum not found");
+		exit(EXIT_FAILURE);
+	}
+
+	// Open the device
+	r = libusb_open(dev, &dev_handle);
+	if (r != 0) {
+		fprintf(stderr, "%s: failed to connect to device", libusb_strerror(r));
+		exit(EXIT_FAILURE);
+	}
+
+	// Now we have a handle and can perform IO on the device
+	r = libusb_claim_interface(dev_handle, 0); // unsure which interface we should be claiming, but only 0 and 1 don't error
+	//r = libusb_detach_kernel_driver(dev_handle, 3);
+	if (r != 0) {
+		fprintf(stderr, "%s: failed to claim interface", libusb_strerror(r));
+		exit(EXIT_FAILURE);
+	}
+
+
+
+	libusb_release_interface(dev_handle, 3);
+	libusb_close(dev_handle);
 	libusb_free_device_list(devs, 1);
 	libusb_exit(NULL);
 
